92cf73b4d2e1b49e5a942ffacd784abaffe8fadd
thmsweaver <thmsweaver@gmail.com>
2014-07-31 13:34:58 -0400

    first commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() {
+
+  $('.candy').click(function() {
+    $('.candy-text').toggleClass('dropdown');
+  });
+
+  $(window).resize(function(){
+    $('.portfolio-piece').css('width', ($(window).width() / 3) + 'px');
+  });
+
+  $('.portfolio-piece').click(function() {
+    $(this).toggleClass('length');
+    $(this).children().eq(1).toggleClass('arrow-down');
+    $(this).nextUntil('li').slideToggle();
+  });
+
+  $('.portfolio').click(function() {
+    $('.contact-dropdown').addClass('dropdown');
+    $('.portfolio-display').addClass('dropdown');
+    $('.portfolio-dropdown').removeClass('dropdown');
+  });
+
+  $('.contact').click(function() {
+    $('.portfolio-dropdown').addClass('dropdown');
+    $('.contact-dropdown').toggleClass('dropdown');
+  });
+
+});
